9edd0a909f634efd946d840bcc0d0617c7785b22
Toroshhin <54998119+Toroshhin@users.noreply.github.com>
2020-10-08 16:58:47 +0300

    add files

---
+++ excercises 1/src/components/InputPhone.js
@@ -0,0 +1,8 @@
+
+import React, { Component } from "react";
+
+import '../styles/inputPhone.css';
+
+function Button(props) {
+  <div className=''></div>
+}
---
+++ excercises 1/src/index.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import InputPhone from "./components/InputPhone.js";
+
+ReactDOM.render(<InputPhone />, document.getElementById("root"));
+
+
+
+
+
