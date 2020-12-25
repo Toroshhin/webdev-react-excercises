0504ab28217e8d10e7f710d529e3ab4067fba7d8
Toroshhin <54998119+Toroshhin@users.noreply.github.com>
2020-12-26 00:51:41 +0300

    Fixed bugs

--- excercises 3/src/components/App.js
+++ excercises 3/src/components/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import WeatherWidget from './WeatherWidget.js'
 import './styles/WeatherWidget.css'
 
+function randomInteger(min, max) {
+  // получить случайное число от (min-0.5) до (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
+let imgArray = ['./public/icon/sun.png', './public/icon/rain.png', './public/icon/shower.png', './public/icon/snow.png', './public/icon/thunderstorm.png']
+
 class App extends React.Component {
   render() {
     return (
-      <WeatherWidget />
+      <WeatherWidget temp={randomInteger(-30, 30)} link={imgArray[randomInteger(0, 4)]} city={'Нижний Новгород'} humidity={randomInteger(50, 80)} precipitation={randomInteger(0, 2)} wind={randomInteger(0, 21)} />
+
     )
   }
 }
 
 export default App
+
+
+
--- excercises 3/src/components/WeatherWidget.js
+++ excercises 3/src/components/WeatherWidget.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 
-class WeatherWidget extends React.Component {
+
+class WeatherWidget extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -9,20 +11,43 @@ class WeatherWidget extends React.Component {
   }
   render() {
     return (
-      <div className={'weather-widget'}>
+      <div className='weather-widget'>
         <div className='weather-widget__top-list'>
-          <div className='weather-widget__temperature'>29"С</div>
-          <div className='weather-widget__city'>Moscow</div>
+          <div className='weather-widget__temperature'>{this.props.temp}°C</div>
+          <div className='weather-widget__city'>{this.props.city}</div>
         </div>
-        <div className='weather-widget__icon'>Здесь значок</div>
-        <div className='weather-widget__bottom-list'>
-          <div className='weather-widget__humidity'>Влажность</div>
-          <div className='weather-widget__pecipitation'>Осадки</div>
-          <div className='weather-widget__speed-wind'>Скорость ветра</div>
+
+        <img className='weather-widget__icon' src={this.props.link} />
+
+        <div className='weather-widget__info'>
+          <div className='weather-widget__humidity'>
+            <div className='weather-widget__text'>Влажность</div>
+            <div className='weather-widget__value'>{this.props.humidity}%</div>
+          </div>
+          <div className='weather-widget__precipitation'>
+            <div className='weather-widget__text'>Осадки</div>
+            <div className='weather-widget__value'>{this.props.precipitation}мм</div>
+          </div>
+          <div className='weather-widget__wind'>
+            <div className='weather-widget__text'>Скорость ветра</div>
+            <div className='weather-widget__value'>{this.props.wind}м/c</div>
+          </div>
         </div>
-      </div>
+
+
+      </div >
     );
   }
 }
+
+WeatherWidget.propTypes = {
+  link: PropTypes.string.isRequired,
+  temp: PropTypes.number.isRequired,
+  city: PropTypes.string.isRequired,
+  humidity: PropTypes.number.isRequired,
+  precipitation: PropTypes.number.isRequired,
+  wind: PropTypes.number.isRequired
+};
+
 
 export default WeatherWidget
